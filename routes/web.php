d630de1a428af9a73777ae7980115a69cf6b35b7
kajal <kajal.dhariya@togglebytes.com>
2023-04-05 10:22:38 +0530

    add blogs page

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 // ]);
 
 Route::statamic('posts', 'posts.index', ['title' => 'Posts']);
-Route::statamic('blogs', 'posts.index', ['title' => 'Posts']);
+Route::statamic('blogs', 'blogs.index', ['title' => 'Blogs']);
 
 Route::statamic('site.webmanifest', 'partials._manifest', [
     'layout' => null,
